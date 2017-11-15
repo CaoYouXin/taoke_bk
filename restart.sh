b56ef66dff9161cee6a079dcec67ae356fb26d2f
曹又心 <c_lisheng@163.com>
2017-11-15 13:57:44 +0800

    profile dev prod

--- restart.sh
+++ restart.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+nginx -s reload
 kill -9 $(lsof -t -i:10001) && nohup java -jar /var/taoke/miquaner-0.0.1-SNAPSHOT.jar --server.port=10001 >/dev/null 2>&1 </dev/null &
--- upload.sh
+++ upload.sh
@@ -2,4 +2,4 @@
 
 rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress /Users/cls/Dev/Git/taoke/taoke_bk/target/miquaner-0.0.1-SNAPSHOT.jar root@www.tkmqr.com:/var/taoke/
 
-rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress /usr/local/etc/nginx/servers/* root@www.tkmqr.com:/etc/nginx/sites-enabled/
+#rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress /usr/local/etc/nginx/servers/* root@www.tkmqr.com:/etc/nginx/sites-enabled/
