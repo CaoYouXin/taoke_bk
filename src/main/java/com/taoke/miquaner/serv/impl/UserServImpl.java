2f0803d55e661a71068a73890b4610445aabf6f8
曹又心 <c_lisheng@163.com>
2017-11-27 14:46:12 +0800

    通过审核后发送端性

--- src/main/java/com/taoke/miquaner/serv/impl/UserServImpl.java
+++ src/main/java/com/taoke/miquaner/serv/impl/UserServImpl.java
@@ -345,7 +345,7 @@ public class UserServImpl implements IUserServ {
 
         List<EAdmin> admins = this.adminRepo.findAllByGrantedAdminsIsNull();
         if (!admins.isEmpty()) {
-            this.msgServ.send2One(admins.get(0), one, "系统消息", "您刚刚申请了成为合伙人！");
+            this.msgServ.send2One(admins.get(0), one, "系统消息", "您刚刚申请了成为合伙人，我们的工作人员会在一个工作日内，完成审批，请耐心等待。");
         }
 
         return Result.success(null);
@@ -373,6 +373,37 @@ public class UserServImpl implements IUserServ {
         List<EAdmin> admins = this.adminRepo.findAllByGrantedAdminsIsNull();
         if (!admins.isEmpty()) {
             this.msgServ.send2One(admins.get(0), one, "系统消息", "恭喜您！通过合伙人审核。");
+        }
+
+        //组装请求对象
+        SendSmsRequest request = new SendSmsRequest();
+        //使用post提交
+        request.setMethod(MethodType.POST);
+        //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
+        request.setPhoneNumbers(one.getPhone());
+        //必填:短信签名-可在短信控制台中找到
+        request.setSignName("觅券儿");
+        //必填:短信模板-可在短信控制台中找到
+        request.setTemplateCode("SMS_113461319");
+        //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+        //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
+//        request.setTemplateParam("{\"code\":\"" + smsCode.getCode() + "\"}");
+        //可选-上行短信扩展码(扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段)
+        //request.setSmsUpExtendCode("90997");
+        //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+        //request.setOutId("yourOutId");
+        //请求失败这里会抛ClientException异常
+        SendSmsResponse sendSmsResponse = null;
+        try {
+            sendSmsResponse = acsClient.getAcsResponse(request);
+        } catch (ClientException e) {
+            logger.error("send sms failed.");
+            return Result.success(view);
+        }
+        if (sendSmsResponse.getCode() != null && sendSmsResponse.getCode().equals("OK")) {
+            logger.info("send sms successfully");
+        } else {
+            logger.error("send sms failed.");
         }
 
         return Result.success(view);
