5c47339ce0f5134ef6053a3616e11f7876a7591a
曹又心 <c_lisheng@163.com>
2017-12-05 10:58:40 +0800

    build script optimized

--- publish.sh
+++ publish.sh
@@ -2,4 +2,20 @@
 
 . ./upload.sh
 
-ssh root@www.tkmqr.com "bash -s" < restart.sh &
+#ssh root@www.tkmqr.com "bash -s" < nginx.sh &
+
+ssh root@www.tkmqr.com "bash -s" < restart.sh 10001 &
+
+echo "sleeping for 5 seconds ..."
+sleep 5
+
+bash ./testurl.sh "http://server.tkmqr.com:10001/admin/ping"
+
+ssh root@www.tkmqr.com "bash -s" < restart.sh 10002 &
+
+echo "sleeping for 5 seconds ..."
+sleep 5
+
+bash ./testurl.sh "http://server.tkmqr.com:10002/admin/ping"
+
+echo "all published"
