58a350ee35a59fb3d110d3112ecd435228744082
曹又心 <c_lisheng@163.com>
2017-12-24 18:16:06 +0800

    build script

--- publish.sh
+++ publish.sh
@@ -6,15 +6,15 @@
 
 ssh root@www.tkmqr.com "bash -s" < restart.sh 10001 &
 
-echo "sleeping for 5 seconds ..."
-sleep 5
+echo "sleeping for 10 seconds ..."
+sleep 10
 
 bash ./testurl.sh "http://server.tkmqr.com:10001/admin/ping"
 
 ssh root@www.tkmqr.com "bash -s" < restart.sh 10002 &
 
-echo "sleeping for 5 seconds ..."
-sleep 5
+echo "sleeping for 10 seconds ..."
+sleep 10
 
 bash ./testurl.sh "http://server.tkmqr.com:10002/admin/ping"
 
